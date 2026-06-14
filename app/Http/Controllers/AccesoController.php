3bf643ca41ca4500fb851da9afc417972979a88d
AndruxDev <andres.ldu@hotmial.com>
2026-06-14 01:09:39 -0500

    borrar \API en controller

--- app/Http/Controllers/AccesoController.php
+++ app/Http/Controllers/AccesoController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreAccesoRequest;
 use App\Http\Resources\AccesoResource;
--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\BaseController;
 use App\Http\Requests\Auth\LoginRequest;
